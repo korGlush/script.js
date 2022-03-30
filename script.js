4d0859d042ffb29e0239c8b993bd6944aaa4c311
Glush <90596091+korGlush@users.noreply.github.com>
2022-03-30 17:32:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-const title = prompt('Как называется ваш проект?');
-const screens = prompt('Какие типы экранов нужно разработать? \n пример: "Простые, Сложные, Интерактивные"');
-const screenPrice = +prompt('колько будет стоить данная работа? \n пример: 12000');
-const adaptive = prompt('Нужен ли адаптив на сайте?');
-const service1 = prompt('Какой дополнительный тип услуги нужен? \n Например: service1, service2');
-const servicePrice1 = +prompt('Сколько это будет стоить? \n Например: 5000');
-const service2 = prompt('Какой дополнительный тип услуги нужен? \n Например: service1, service2');
-const servicePrice2 = +prompt('Сколько это будет стоить? \n Например: 3500');
-const rollBack = .9;
+let btn = document.querySelector('#btn')
+let text = document.querySelector('#text')
+let range = document.querySelector('#range')
+let circle = document.querySelector('#circle')
+let rangeSpan = document.querySelector('#range-span')
+let e_btn = document.querySelector('#e_btn')
 
-
-let fullPrice = screenPrice + servicePrice1 + servicePrice2
-console.log(fullPrice);
-let servicePercentPrice = Math.ceil(fullPrice * rollBack);
-console.log(servicePercentPrice);
-
-console.log(title); 
-console.log(screens);
-console.log(screenPrice);
-console.log(adaptive);
-
-if (adaptive == "да") {
-    console.log(Boolean(1));
-} else if (adaptive == "нет") {
-    console.log(Boolean(""));
+function start() {
+    rangeSpan.textContent = 50
+    circle.style.height = range.value + '%'
+    circle.style.width = range.value + '%'
+    e_btn.style.display = 'none'
 }
 
-if (fullPrice > 0 && fullPrice < 15000) {
-    console.log("Скидка не предусмотрена");
-} else if (fullPrice > 15000 && fullPrice < 30000) {
-    console.log("Даем скидку в 5%");
-} else if (fullPrice > 30000) {
-    console.log("Даем скидку в 10%");
-} else if (fullPrice < 0) {
-    console.log("Что то пошло не так");
+
+const color = function() {
+    btn.style.backgroundColor = text.value
 }
 
+
+const position = function(){
+    circle.style.height = range.value + '%'
+    circle.style.width = range.value + '%'
+    rangeSpan.textContent = event.target.value
+}
+
+start()
+btn.addEventListener('click', color)
+range.addEventListener('input', position)
