194c2680a6c401390c43f22af8fb49916ddd5ff4
Glush <90596091+korGlush@users.noreply.github.com>
2022-04-01 15:36:10 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,50 @@
-title = document.getElementsByTagName('h1')
-console.log(title);
-button = document.getElementsByClassName('handler_btn')
-console.log(button);
-buttonTiming = document.querySelector('.screen-btn')
-console.log(buttonTiming);
-percent = document.querySelectorAll('.other-items.percent')
-number = document.querySelectorAll('.other-items.number')
-console.log(percent,number);
-range = document.querySelector('.rollback input')
-console.log(range);
-span = document.querySelector('.rollback .range-value')
-console.log(span);
-input = document.getElementsByClassName('total-input')
-console.log(input);
-let = document.querySelectorAll('.screen')
-console.log(let);
+const todoControl = document.querySelector('.todo-control')
+const headerInput = document.querySelector('.header-input')
+const todoList = document.queryCommandEnabled('.todo-list')
+const todoCompleted = document.queryCommandEnabled('.todo-completed')
+
+const toDoData = []
+
+const render = function () {
+    todoList.innerHTML = ''
+    todoCompletedt.innerHTML = ''
+    toDoData.forEach(function (item) {
+   
+        const li = document.createElement('li')
+
+        li.classList.add('todo-item')
+
+        li.innerHTML = '<span class="text-todo'> + item.text + '</span>' +
+            '<div class="todo-buttons">' +
+		    '<button class="todo-remove"></button>' +
+		    '<button class="todo-complete"></button>' +
+		    '</div>'
+        if(item.completed) {
+            todoCompleted.append(li)
+        } else {
+            todoList.append(li)
+        }
+
+        li.querySelector('.todo-complete').addEventListener('click' , function(){
+            item.completed = !item.completed
+            render()
+        })
+        
+    })
+}
+
+todoControl.addEventListener('sumbit' , function (event) {
+    event.preventDefault()
+
+    const newToDo = {
+        text: headerInput.nodeValue,
+        completed: false
+    }
+
+    toDoData.push(newToDo)
+    headerInput.value = ''
+
+
+    render()
+
+})
