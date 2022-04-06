60a4ae9fe99674f233eeb63865b3f1d33e9f9dc2
Glush <90596091+korGlush@users.noreply.github.com>
2022-04-06 13:26:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,77 @@
-const input = document.getElementById('input')
-const btnSave = document.getElementById('btn_save')
-const btnRemove = document.getElementById('btn_remove')
-const text = document.getElementById('text')
+const todoControl = document.querySelector(".todo-control")
+const headerInput = document.querySelector(".header-input")
+const todoList = document.querySelector(".todo-list")
+const todoCompleted = document.querySelector(".todo-completed")
+const todoContainer = document.querySelector(".todo-container")
 
-const showText = function () {
-    text.textContent = sessionStorage.getItem('text')
+let toDoData = []
 
+const render = function () {
+  todoList.innerHTML = ""
+  todoCompleted.innerHTML = ""
+
+  toDoData.forEach(function (item, i) {
+    const li = document.createElement("li")
+
+    li.classList.add("todo-item")
+
+    li.innerHTML =
+      '<span class="text-todo">' +
+      item.text +
+      "</span>" +
+      '<div class="todo-buttons">' +
+      '<button class="todo-remove"></button>' +
+      '<button class="todo-complete"></button>' +
+      "</div>"
+
+    if (item.completed) {
+      todoCompleted.append(li)
+    } else {
+      todoList.append(li)
+    }
+
+    li.querySelector(".todo-complete").addEventListener("click", function () {
+      item.completed = !item.completed
+      render()
+    })
+
+    li.querySelector(".todo-remove").addEventListener("click", function () {
+      li.remove()
+      toDoData.splice(i, 1)
+      render()
+      localStorage.setItem("todoList", JSON.stringify(toDoData))
+      if (toDoData.length === 0) {
+        localStorage.clear()
+      }
+    })
+    if (toDoData.length >= 1) {
+      localStorage.setItem("todoList", JSON.stringify(toDoData))
+    }
+  })
 }
 
-btnSave.addEventListener('click', function() {
-    sessionStorage.setItem('text' , input.value)
-    showText()
-})
+const check = () => {
+  toDoData = localStorage.getItem("todoList")
+    ? JSON.parse(localStorage.getItem("todoList"))
+    : []
+  render()
+}
 
-btnRemove.addEventListener('click', function() {
-    localStorage.clear()
-    sessionStorage.clear()
-    showText()
-})
+todoControl.addEventListener("submit", function (event) {
+  event.preventDefault()
 
-showText()
+  headerInput.value = headerInput.value.trim()
+
+  if (headerInput.value.length !== 0) {
+    const newToDo = {
+      text: headerInput.value,
+      completed: false,
+    }
+
+    toDoData.push(newToDo)
+
+    headerInput.value = ""
+    render()
+  }
+})
+check()
